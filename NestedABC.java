a8d636c2caac0b802235ce3f269fc7502347dc86
prathapxsz <prathapxsz@gmail.com>
2019-12-31 13:31:05 +0530

    testing git

---
+++ NestedABC.java
@@ -0,0 +1,14 @@
+
+public class NestedABC {
+	public static void main(String[] args)
+	{
+		for(int i=65;i<=68;i++)
+		{
+	      for(int j=65;j<=i;j++)
+	      {
+	    	  System.out.print((char)j + " ");
+	      }
+	      System.out.println();
+		}
+	}
+}
